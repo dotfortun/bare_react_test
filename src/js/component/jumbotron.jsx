3d4e62d74fe17b683e09c043401478e81d56d55e
dotfortun <being.for.itself@gmail.com>
2022-02-14 00:14:55 +0000

    Tweaks

--- src/js/component/jumbotron.jsx
+++ src/js/component/jumbotron.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Jumbotron = (props) => {
 	return (
-		<div class="container-fluid py-5 text-white bg-dark">
+		<div class="container-fluid py-5 text-white bg-secondary">
 			<h1 class="display-5 fw-bold">{props.title}</h1>
 			<p class="col-md-8 fs-4">{props.text}</p>
 			<button class="btn btn-primary btn-lg" type="button">
